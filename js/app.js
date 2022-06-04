5f2735f554be35fc99ff91ebf515fb7c4569b6b3
afuza pratama <afuzapratama@gmail.com>
2022-06-04 21:24:13 +0800

    Finish Afuza

---
+++ js/app.js
@@ -0,0 +1,49 @@
+/* -----------------------------------------------
+/* How to use? : Check the GitHub README
+/* ----------------------------------------------- */
+
+/* To load a config file (particles.json) you need to host this demo (MAMP/WAMP/local)... */
+/*
+particlesJS.load('particles-js', 'particles.json', function() {
+  console.log('particles.js loaded - callback');
+});
+*/
+
+/* Otherwise just put the config content (json): */
+
+particlesJS("snowfall", {
+  "particles": {
+    "number": {
+      "value": 100
+    },
+    "shape": {
+      "type": "circle"
+    },
+    "size": {
+      "value": 10,
+      "random": true
+    },
+    "line_linked": {
+      "enable": false
+    },
+    "move": {
+      "enable": true,
+      "speed": 2,
+      "direction": "bottom",
+      "straight": false
+    }
+  },
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": {
+        "enable": false
+      }
+    },
+    "modes": {
+      "push": {
+        "particles_nb": 12
+      }
+    }
+  }
+});
